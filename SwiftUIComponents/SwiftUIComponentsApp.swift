c0aa3e5d28683c7916068fa3df7482a6c1ff544e
Ruwan Thalagaha Gedara <ruwantgs@gmail.com>
2024-07-18 12:23:21 -0400

    Initial commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Ruwan Thalgahage on 2024-07-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
